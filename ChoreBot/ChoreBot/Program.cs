2a0672b5fa5ba528230eed27ca1947d4e7f9df7c
Alex King <alex.king.60137@gmail.com>
2022-10-19 20:42:08 -0400

    Connect to Discord

--- ChoreBot/ChoreBot/Program.cs
+++ ChoreBot/ChoreBot/Program.cs
@@ -1,10 +1,46 @@
-﻿namespace ChoreBot
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace ChoreBot
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private DiscordSocketClient _client;
+
+        public Program()
         {
-            Console.WriteLine("Hello, World!");
+            _client = new DiscordSocketClient();
+        }
+
+        public static Task Main(string[] args) => new Program().MainAsync();
+
+        public async Task MainAsync()
+        {
+
+            _client.Log += Log;
+            string token = GetBotToken();
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        private static string GetBotToken()
+        {
+            // @Dave, Hit me up to get the token.
+            var token = File.ReadAllText("token.txt");
+            return token;
+            // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
+            // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
+            // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
         }
     }
 }
