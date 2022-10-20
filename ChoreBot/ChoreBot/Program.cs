968264e10a265702aa8441e822befcc1a5344d26
Alex King <alex.king.60137@gmail.com>
2022-10-19 21:30:37 -0400

    TEXT MESSAGES ARE FOR LOSERS, WE SLASH COMMANDS NOW BOIIIIIIIII

--- ChoreBot/ChoreBot/Program.cs
+++ ChoreBot/ChoreBot/Program.cs
@@ -1,7 +1,9 @@
 ﻿using Discord;
 using Discord.Commands;
+using Discord.Net;
 using Discord.WebSocket;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
 using System.Reflection;
 using System.Windows.Input;
 
@@ -86,7 +88,30 @@ namespace ChoreBot
             // Note that the first one is 'Modules' (plural) and the second is 'Module' (singular).
 
             // Subscribe a handler to see if a message invokes a command.
-            _client.MessageReceived += HandleCommandAsync;
+            //_client.MessageReceived += HandleCommandAsync;
+            _client.Connected += Client_Ready;
+            _client.SlashCommandExecuted+= HandleCommandAsync;
+        }
+
+        public async Task Client_Ready()
+        {
+            var testCommand = new SlashCommandBuilder();
+            testCommand.WithName("test");
+            testCommand.WithDescription("A test command to see what happens");
+
+            try
+            {
+                await _client.CreateGlobalApplicationCommandAsync(testCommand.Build());
+            }
+            catch (HttpException exception)
+            {
+                // If our command was invalid, we should catch an ApplicationCommandException. This exception contains the path of the error as well as the error message. You can serialize the Error field in the exception to get a visual of where your error is.
+                var json = JsonConvert.SerializeObject(exception.Errors, Formatting.Indented);
+
+                // You can send this error somewhere or just print it to the console, for this example we're just going to print it.
+                Console.WriteLine(json);
+            }
+
         }
 
         // If any services require the client, or the CommandService, or something else you keep on hand,
@@ -94,10 +119,10 @@ namespace ChoreBot
         // If this method is getting pretty long, you can seperate it out into another file using partials.
         private static IServiceProvider ConfigureServices()
         {
-            var map = new ServiceCollection();
+            var map = new ServiceCollection()
                 // Repeat this for all the service classes
                 // and other dependencies that your commands might need.
-                //.AddSingleton(new SomeServiceClass());
+                .AddSingleton<CommandHandler>();
 
             // When all your required services are in the collection, build the container.
             // Tip: There's an overload taking in a 'validateScopes' bool to make sure
@@ -105,37 +130,9 @@ namespace ChoreBot
             return map.BuildServiceProvider();
         }
 
-        private async Task HandleCommandAsync(SocketMessage arg)
+        private async Task HandleCommandAsync(SocketSlashCommand command)
         {
-            // Bail out if it's a System Message.
-            var msg = arg as SocketUserMessage;
-            if (msg == null) return;
-
-            // We don't want the bot to respond to itself or other bots.
-            if (msg.Author.Id == _client.CurrentUser.Id || msg.Author.IsBot) return;
-
-            // Create a number to track where the prefix ends and the command begins
-            int pos = 0;
-            // Replace the '!' with whatever character
-            // you want to prefix your commands with.
-            // Uncomment the second half if you also want
-            // commands to be invoked by mentioning the bot instead.
-            if (msg.HasCharPrefix('!', ref pos) /* || msg.HasMentionPrefix(_client.CurrentUser, ref pos) */)
-            {
-                // Create a Command Context.
-                var context = new SocketCommandContext(_client, msg);
-
-                // Execute the command. (result does not indicate a return value, 
-                // rather an object stating if the command executed successfully).
-                var result = await _commands.ExecuteAsync(context, pos, _services);
-
-                // Uncomment the following lines if you want the bot
-                // to send a message if it failed.
-                // This does not catch errors from commands with 'RunMode.Async',
-                // subscribe a handler for '_commands.CommandExecuted' to see those.
-                //if (!result.IsSuccess && result.Error != CommandError.UnknownCommand)
-                //    await msg.Channel.SendMessageAsync(result.ErrorReason);
-            }
+            await command.RespondAsync($"You executed {command.Data.Name}");
         }
 
         private static string GetBotToken()
