013fde3d2951e96f44f3d7cbc7d16bc66cd8bd09
Alex King <alex.king.60137@gmail.com>
2022-11-06 10:15:42 -0500

    Respond to @Mentions and Messages now.

--- ChoreBot/ChoreBot/Commands/TestPingCommand.cs
+++ ChoreBot/ChoreBot/Commands/TestPingCommand.cs
@@ -1,51 +1,105 @@
 ﻿using Discord;
+using Discord.Rest;
 using Discord.WebSocket;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ChoreBot.Commands
 {
     internal class TestPingCommand : BaseDiscordCommand
     {
+        private const int targetArguementIndex = 0;
+        private const int messageArguementIndex = 1;
+
         public override string Name => "ping";
 
         public override string Description => "Ping someone";
 
         public override ApplicationCommandProperties BuildCommand()
         {
-            SlashCommandOptionBuilder slashCommandOptionBuilder = new();
-            slashCommandOptionBuilder.WithName("target");
-            slashCommandOptionBuilder.WithType(ApplicationCommandOptionType.String);
-            slashCommandOptionBuilder.WithDescription("Target to Ping");
-            slashCommandOptionBuilder.WithRequired(true); // Only add this if you want it to be required
+            SlashCommandOptionBuilder targetCommandOptionBuilder = new();
+            targetCommandOptionBuilder.WithName("target");
+            targetCommandOptionBuilder.WithType(ApplicationCommandOptionType.String);
+            targetCommandOptionBuilder.WithDescription("Target to Ping");
+            targetCommandOptionBuilder.WithRequired(true); // Only add this if you want it to be required
+            
+            SlashCommandOptionBuilder messageOptionBuilder = new();
+            messageOptionBuilder.WithName("message");
+            messageOptionBuilder.WithType(ApplicationCommandOptionType.String);
+            messageOptionBuilder.WithDescription("Message for Target");
 
-            var echoCommandBuilder = new SlashCommandBuilder();
-            echoCommandBuilder.WithName(Name);
-            echoCommandBuilder.WithDescription(Description);
-            echoCommandBuilder.AddOptions(slashCommandOptionBuilder);
+            var pingCommandBuilder = new SlashCommandBuilder();
+            pingCommandBuilder.WithName(Name);
+            pingCommandBuilder.WithDescription(Description);
+            pingCommandBuilder.AddOptions(targetCommandOptionBuilder, messageOptionBuilder);
 
-            return echoCommandBuilder.Build();
+            return pingCommandBuilder.Build();
         }
 
         protected override async Task RunCommandAsync(SocketSlashCommand command)
         {
-            //await command.RespondAsync($"@{command.Data.Options.First().Value}");
-            var userTarget = (string)command.Data.Options.First().Value ?? string.Empty;
             
+            var options = command.Data.Options.ToList();
+            var userTarget = (string)options[targetArguementIndex].Value ?? string.Empty;
 
             var users = await command.Channel.GetUsersAsync().FlattenAsync();
             var userWithName = users.SingleOrDefault(user => user.Username == userTarget);
-            if (userWithName is null)
+            
+            var isUserNameAMention = IsMention(userTarget);
+            var isActiveUser = userWithName is not null;
+            var userFound = isActiveUser || isUserNameAMention;
+            if (!userFound)
             {
-                await command.RespondAsync($"Could not find user with name: {command.Data.Options.First().Value}");
+                await command.RespondAsync($"Could not find user: {userTarget} (Try using an @Mention)");
                 return;
             }
 
-            await command.RespondAsync(userWithName.Mention);
+            var userNameToMention = string.Empty;
+            if (isActiveUser)
+            {
+                userNameToMention = userWithName?.Mention;
+            }
+            else if (isUserNameAMention)
+            {
+                userNameToMention = userTarget;
+            }
 
+            //if message present send it as well
+            string messageArgument = BuildMessageFromArgument(options);
+            var message = string.Join(' ', userNameToMention, messageArgument).Trim();
+
+            await command.RespondAsync(message);
+
+        }
+
+
+        private Regex MentionSyntax = new Regex("^<@.*>");
+        private bool IsMention(string userTarget)
+        {
+            return MentionSyntax.IsMatch(userTarget);
+        }
+
+        private string BuildMessageFromArgument(List<SocketSlashCommandDataOption> options)
+        {
+            string messageArguement = string.Empty;
+            try
+            {
+                if (options.Count > messageArguementIndex)
+                {
+                    messageArguement = (string)options[messageArguementIndex].Value ?? string.Empty;
+
+                }
+            }
+            catch
+            {
+                // message doesnt exist
+            }
+
+            return messageArguement;
         }
     }
 }
